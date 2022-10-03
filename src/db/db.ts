ee9e3bda5c5d9bdfbe6435310ef048a794ae909c
NataliaFrancisca <nathifrancisca@gmail.com>
2022-10-03 12:03:59 -0300

    feat: update db

---
+++ src/db/db.ts
@@ -0,0 +1,42 @@
+export const fakeData = [
+    {
+        nome: "Lágrimas",
+        artista: "Baco Exu do Blues, Gal Costa",
+        album: "QVVJFA",
+        anoLancamento: 2022,
+        favoritar: true
+    },
+
+    {
+        nome: "Eu Não Te Amo",
+        artista: "Don L, Diomedes Chinaski",
+        album: "Roteiro para Ainouz",
+        anoLancamento: 2017,
+        favoritar: true,
+    },
+
+    {
+        nome: "Oceano",
+        artista: "Mc Tha",
+        album: "Rito de Passá",
+        anoLancamento: 2019,
+        favoritar: true
+    },
+
+    {
+        nome: "Visão",
+        artista: "BK",
+        album: "O Líder em Movimento",
+        anoLancamento: 2019,
+        favoritar: true
+    },
+
+    {
+        nome: "Eu amo você",
+        artista: "Tim Maia",
+        album: "Tim Maia 1970",
+        anoLancamento: 1970,
+        favoritar: false
+    }
+]
+
